1fe327da4b4a03e05d0bb422daaabd112219d24f
khasan0102 <khasanabdukarimov1@gmail.com>
2021-08-19 10:45:06 +0500

    added pagination

--- src/modules/admin/group/model.js
+++ src/modules/admin/group/model.js
@@ -2,16 +2,14 @@ const { fetch, fetchAll } = require('../../../lib/postgres.js');
 
 const { GROUPS, COUNT_GROUPS } = require('./query.js');
 
-const groups = async ({ page }) => {
-    page = +page;
+const groups = async ({ page = 1 }) => {
+    page = +page
+    let { count } = await fetch(COUNT_GROUPS);
+    let limit = 1;
     let pages = Math.ceil(count / limit);
     if(page > pages) page = pages;
     if(page < 1) page = 1;
-
-    let { count } = await fetch(COUNT_GROUPS);
-    let limit = 2;
 	let groups = await fetchAll(GROUPS, (page - 1) * limit, limit);
-
     return {
         html: 'private/admin.html',
         panel: 'table-groups.html',
